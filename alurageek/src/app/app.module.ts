10a07e2c1a623c6acf01a86853d96452101c4bc6
Breno <brenogpa@gmail.com>
2022-10-06 17:31:58 -0300

    created header

--- alurageek/src/app/app.module.ts
+++ alurageek/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HomeModule } from './home/home.module';
+import { HeaderModule } from './components/header/header.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +14,7 @@ import { HomeModule } from './home/home.module';
     AppRoutingModule,
     NgbModule,
     FontAwesomeModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent],
