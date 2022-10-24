7c9304100fc23fc1ccdbc54e360ff6ba2278d2fe
Breno <brenogpa@gmail.com>
2022-10-24 11:01:25 -0300

    installed nodemon/cors

--- api/index.js
+++ api/index.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const server = express();
 const users = require('./src/data/users.json');
-const products = require('./src/data/products.json');
 
-server.get('/users', (req,res) => {
+server.get('/usuarios', (req,res) => {
     return res.json(users)
-})
-server.get('/products', (req,res) => {
-    return res.json(products)
 })
 
 server.listen(3000, () => {
     console.log('Servidor funcionando (ouvindo na porta 3000)')
 });
+
