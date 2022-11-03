e367d696a1be82e4b637199f73e183d55081d732
Breno Abreu <breno.abreu@controplan.com.br>
2022-11-03 16:33:12 -0300

    conexão com mongodb

--- api/src/app.js
+++ api/src/app.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const index = require("./routes/index-route");
-const routes = require("./routes/product-routes");
+const mongoose = require("mongoose");
 
 const app = express();
 const router = express.Router();
 
+// Conecta ao banco
+mongoose.connect(
+  "mongodb+srv://brenogpa:bb1703@bstorage.kfumj4o.mongodb.net/?retryWrites=true&w=majority"
+);
+
+// Carrega os models
+const Product = require("./models/product");
+
+// Carrega as rotas
+const indexRoute = require("./routes/index-route");
+const productRoute = require("./routes/product-routes");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/", index);
-app.use("/routes", routes);
+app.use("/", indexRoute);
+app.use("/products", productRoute);
 
 module.exports = app;
