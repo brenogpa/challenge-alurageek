465b78902932dd5156e1206170a76090e8871c35
Breno Abreu <breno.abreu@controplan.com.br>
2022-11-04 16:16:18 -0300

    atualizados métodos put e delete de products

--- api/src/controllers/product-controller.js
+++ api/src/controllers/product-controller.js
@@ -68,12 +68,40 @@ exports.post = (req, res, next) => {
 //------------Métodos PUT---------------
 
 exports.put = (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send({ id: id, item: req.body });
+  Product.findByIdAndUpdate(req.params.id, {
+    $set: {
+      title: req.body.title,
+      description: req.body.description,
+      slug: req.body.slug,
+      price: req.body.price,
+    },
+  })
+    .then((x) => {
+      res.status(200).send({
+        message: "Produto atualizado com sucesso!",
+      });
+    })
+    .catch((e) => {
+      res.status(400).send({
+        message: "Falha ao atualizar o produto.",
+        data: e,
+      });
+    });
 };
 
-//------------Métodos DEL---------------
+//------------Métodos DELETE---------------
 
 exports.delete = (req, res, next) => {
-  res.status(200).send(req.body);
+  Product.findOneAndRemove(req.body.id)
+    .then((x) => {
+      res.status(200).send({
+        message: "Produto removido com sucesso!",
+      });
+    })
+    .catch((e) => {
+      res.status(400).send({
+        message: "Falha ao remover o produto.",
+        data: e,
+      });
+    });
 };
