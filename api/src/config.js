fff73ed260508d8588f4dd5fec68d1ef88edb703
Breno <brenogpa@gmail.com>
2022-11-09 17:09:52 -0300

    password config

---
+++ api/src/config.js
@@ -0,0 +1,7 @@
+global.SALT_KEY = "bw3b543grfgj-g4wt4t54223ges-gr4336738ge43kj5-jrhe5e";
+
+module.exports = {
+  connectionString:
+    "mongodb+srv://brenogpa:bb1703@bstorage.kfumj4o.mongodb.net/?retryWrites=true&w=majority",
+  containerConnectionString: "TBD",
+};
--- api/src/controllers/user-controller.js
+++ api/src/controllers/user-controller.js
@@ -1,8 +1,13 @@
 const repository = require("../repositories/user-repository");
+const md5 = require("md5");
 
 exports.post = async (req, res, next) => {
   try {
-    await repository.create(req.body);
+    await repository.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: md5(req.body.password + global.SALT_KEY)
+    });
     res.status(201).send({ message: "Usuário cadastrado com sucesso!" });
   } catch (e) {
     res.status(500).send({
